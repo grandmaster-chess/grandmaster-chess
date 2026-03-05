eb04b4dddde6fb24e466e0904d8c613686d682b3
grandmaster-chess <schmloga@hssdschools.org>
2026-03-05 09:30:38 -0600

    Update app.js

--- app.js
+++ app.js
@@ -17,3 +17,12 @@ document.getElementById("moves").innerHTML=""
 }
 
 document.getElementById("newGame").onclick=newGame
+
+document.getElementById("pgnFile").onchange = function(e){
+    const file = e.target.files[0]
+    const reader = new FileReader()
+    reader.onload = function(ev){
+        loadPGN(ev.target.result)
+    }
+    reader.readAsText(file)
+}
